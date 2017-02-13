2560750c08f0c66e5028c3d115cf73c2a1e7888f
ChernetsovNG <n.chernetsov86@gmail.com>
2017-02-13 19:56:28 +0300

    Add user and role classes

---
+++ src/main/java/com/nchernetsov/fintracking/model/Role.java
@@ -0,0 +1,13 @@
+package com.nchernetsov.fintracking.model;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority{
+    ROLE_USER,
+    ROLE_ADMIN;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
+}
