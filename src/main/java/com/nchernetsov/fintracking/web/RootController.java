937fcc49ceeefc341123a32648b862dc778d1de4
ChernetsovNG <n.chernetsov86@gmail.com>
2017-02-17 22:40:43 +0300

    Add RootController

---
+++ src/main/java/com/nchernetsov/fintracking/web/RootController.java
@@ -0,0 +1,28 @@
+package com.nchernetsov.fintracking.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RootController {
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:deposits";
+    }
+
+    @GetMapping("/users")
+    public String users() {
+        return "users";
+    }
+
+    @GetMapping(value = "/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/deposits")
+    public String deposits() {
+        return "deposits";
+    }
+}
