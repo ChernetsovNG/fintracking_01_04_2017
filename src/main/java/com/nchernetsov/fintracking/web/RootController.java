410edd46bdc8baca836e3bbde01f4fde323825f9
ChernetsovNG <n.chernetsov86@gmail.com>
2017-02-20 23:17:38 +0300

    Add some different controllers

--- src/main/java/com/nchernetsov/fintracking/web/RootController.java
+++ src/main/java/com/nchernetsov/fintracking/web/RootController.java
@@ -1,16 +1,28 @@
 package com.nchernetsov.fintracking.web;
 
+import com.nchernetsov.fintracking.AuthorizedUser;
+import com.nchernetsov.fintracking.to.UserTo;
+import com.nchernetsov.fintracking.util.UserUtil;
+import com.nchernetsov.fintracking.web.user.AbstractUserController;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.support.SessionStatus;
+
+import javax.validation.Valid;
 
 @Controller
-public class RootController {
+public class RootController extends AbstractUserController {
 
     @GetMapping("/")
     public String root() {
         return "redirect:deposits";
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/users")
     public String users() {
         return "users";
@@ -25,4 +37,41 @@ public class RootController {
     public String deposits() {
         return "deposits";
     }
+
+    @GetMapping("/profile")
+    public String profile() {
+        return "profile";
+    }
+
+    @PostMapping("/profile")
+    public String updateProfile(@Valid UserTo userTo, BindingResult result, SessionStatus status) {
+        if (result.hasErrors()) {
+            return "profile";
+        } else {
+            super.update(userTo);
+            AuthorizedUser.get().update(userTo);
+            status.setComplete();
+            return "redirect:deposits";
+        }
+    }
+
+    @GetMapping("/register")
+    public String register(ModelMap model) {
+        model.addAttribute("userTo", new UserTo());
+        model.addAttribute("register", true);
+        return "profile";
+    }
+
+    @PostMapping("/register")
+    public String saveRegister(@Valid UserTo userTo, BindingResult result, SessionStatus status, ModelMap model) {
+        if (result.hasErrors()) {
+            model.addAttribute("register", true);
+            return "profile";
+        } else {
+            super.create(UserUtil.createNewFromTo(userTo));
+            status.setComplete();
+            return "redirect:login?message=app.registered&username=" + userTo.getEmail();
+        }
+    }
+
 }
