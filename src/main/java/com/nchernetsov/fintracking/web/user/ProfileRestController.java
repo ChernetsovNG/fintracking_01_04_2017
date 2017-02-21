56d5b44e7de41b1b9938fc4c3a88c577104a793e
ChernetsovNG <n.chernetsov86@gmail.com>
2017-02-21 22:53:33 +0300

    Add AdminRestController tests

---
+++ src/main/java/com/nchernetsov/fintracking/web/user/ProfileRestController.java
@@ -0,0 +1,30 @@
+package com.nchernetsov.fintracking.web.user;
+
+import com.nchernetsov.fintracking.AuthorizedUser;
+import com.nchernetsov.fintracking.model.User;
+import com.nchernetsov.fintracking.to.UserTo;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(ProfileRestController.REST_URL)
+public class ProfileRestController extends AbstractUserController {
+    static final String REST_URL = "/rest/profile";
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public User get() {
+        return super.get(AuthorizedUser.id());
+    }
+
+    @DeleteMapping
+    public void delete() {
+        super.delete(AuthorizedUser.id());
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @Override
+    public void update(@RequestBody UserTo userTo) {
+        super.update(userTo);
+    }
+}
+
---
+++ src/test/java/com/nchernetsov/fintracking/web/user/AdminRestControllerTest.java
@@ -0,0 +1,121 @@
+package com.nchernetsov.fintracking.web.user;
+
+import com.nchernetsov.fintracking.TestUtil;
+import com.nchernetsov.fintracking.model.Role;
+import com.nchernetsov.fintracking.model.User;
+import com.nchernetsov.fintracking.web.AbstractControllerTest;
+import com.nchernetsov.fintracking.web.json.JsonUtil;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static com.nchernetsov.fintracking.TestUtil.userHttpBasic;
+import static com.nchernetsov.fintracking.UserTestData.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class AdminRestControllerTest extends AbstractControllerTest {
+
+    private static final String REST_URL = AdminRestController.REST_URL + '/';
+
+    @Test
+    public void testGet() throws Exception {
+        mockMvc.perform(get(REST_URL + ADMIN_ID)
+                .with(userHttpBasic(ADMIN)))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(MATCHER.contentMatcher(ADMIN));
+    }
+
+    @Test
+    public void testGetNotFound() throws Exception {
+        mockMvc.perform(get(REST_URL + 1)
+                .with(userHttpBasic(ADMIN)))
+                .andExpect(status().isUnprocessableEntity())
+                .andDo(print());
+    }
+
+    @Test
+    public void testGetByEmail() throws Exception {
+        mockMvc.perform(get(REST_URL + "by?email=" + ADMIN.getEmail())
+                .with(userHttpBasic(ADMIN)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(MATCHER.contentMatcher(ADMIN));
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+        mockMvc.perform(delete(REST_URL + USER_ID)
+                .with(userHttpBasic(ADMIN)))
+                .andDo(print())
+                .andExpect(status().isOk());
+        MATCHER.assertCollectionEquals(Arrays.asList(ADMIN, N_Ch), userService.getAll());
+    }
+
+    @Test
+    public void testDeleteNotFound() throws Exception {
+        mockMvc.perform(delete(REST_URL + 1)
+                .with(userHttpBasic(ADMIN)))
+                .andExpect(status().isUnprocessableEntity())
+                .andDo(print());
+    }
+
+    @Test
+    public void testGetUnauthorised() throws Exception {
+        mockMvc.perform(get(REST_URL))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    public void testGetForbidden() throws Exception {
+        mockMvc.perform(get(REST_URL)
+                .with(userHttpBasic(USER)))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        User updated = new User(USER);
+        updated.setName("UpdatedName");
+        updated.setRoles(Collections.singletonList(Role.ROLE_ADMIN));
+        mockMvc.perform(put(REST_URL + USER_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(userHttpBasic(ADMIN))
+                .content(JsonUtil.writeValue(updated)))
+                .andExpect(status().isOk());
+
+        MATCHER.assertEquals(updated, userService.get(USER_ID));
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+        User expected = new User(null, "New", "new@gmail.com", "newPass", Role.ROLE_USER, Role.ROLE_ADMIN);
+        ResultActions action = mockMvc.perform(post(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(userHttpBasic(ADMIN))
+                .content(JsonUtil.writeValue(expected))).andExpect(status().isCreated());
+
+        User returned = MATCHER.fromJsonAction(action);
+        expected.setId(returned.getId());
+
+        MATCHER.assertEquals(expected, returned);
+        MATCHER.assertCollectionEquals(Arrays.asList(ADMIN, N_Ch, expected, USER), userService.getAll());
+    }
+
+    @Test
+    public void testGetAll() throws Exception {
+        TestUtil.print(mockMvc.perform(get(REST_URL)
+                .with(userHttpBasic(ADMIN)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(MATCHER.contentListMatcher(ADMIN, N_Ch, USER)));
+    }
+
+}
