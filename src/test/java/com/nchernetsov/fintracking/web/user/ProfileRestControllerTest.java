8f7eabc793a4fd114c92818ffbb090e4106c7233
ChernetsovNG <n.chernetsov86@gmail.com>
2017-02-22 19:54:56 +0300

    Add ProfileRestController tests

---
+++ src/test/java/com/nchernetsov/fintracking/web/user/ProfileRestControllerTest.java
@@ -0,0 +1,64 @@
+package com.nchernetsov.fintracking.web.user;
+
+
+import com.nchernetsov.fintracking.TestUtil;
+import com.nchernetsov.fintracking.model.User;
+import com.nchernetsov.fintracking.to.UserTo;
+import com.nchernetsov.fintracking.util.UserUtil;
+import com.nchernetsov.fintracking.web.AbstractControllerTest;
+import com.nchernetsov.fintracking.web.json.JsonUtil;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+
+import java.util.Arrays;
+
+import static com.nchernetsov.fintracking.TestUtil.userHttpBasic;
+import static com.nchernetsov.fintracking.UserTestData.*;
+import static com.nchernetsov.fintracking.web.user.ProfileRestController.REST_URL;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class ProfileRestControllerTest extends AbstractControllerTest {
+
+    @Test
+    public void testGet() throws Exception {
+        TestUtil.print(mockMvc.perform(get(REST_URL)
+                .with(userHttpBasic(USER)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(MATCHER.contentMatcher(USER)));
+    }
+
+    @Test
+    public void testGetUnauthorized() throws Exception {
+        mockMvc.perform(get(REST_URL))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+        mockMvc.perform(delete(REST_URL)
+                .with(userHttpBasic(USER)))
+                .andExpect(status().isOk());
+
+        MATCHER.assertCollectionEquals(Arrays.asList(ADMIN, N_Ch), userService.getAll());
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        UserTo updatedTo = new UserTo(null, "newName", "newemail@ya.ru", "newPassword");
+
+        mockMvc.perform(put(REST_URL).contentType(MediaType.APPLICATION_JSON)
+                .with(userHttpBasic(USER))
+                .content(JsonUtil.writeValue(updatedTo)))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        MATCHER.assertEquals(UserUtil.updateFromTo(new User(USER), updatedTo), userService.getByEmail("newemail@ya.ru"));
+    }
+
+}
+
+
