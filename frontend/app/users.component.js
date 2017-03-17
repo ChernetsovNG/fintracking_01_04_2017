b17d543509c3ed96b1eeb4b3086a0853d1f9c8a6
ChernetsovNG <n.chernetsov86@gmail.com>
2017-03-17 20:46:38 +0300

    Add prototype for delete User

--- frontend/app/users.component.js
+++ frontend/app/users.component.js
@@ -33,6 +33,18 @@ System.register(['angular2/core', 'angular2/router', './user.service'], function
                     this._service.getUsers()
                         .subscribe(function (users) { return _this.users = users; });
                 };
+                UsersComponent.prototype.deleteUser = function (user) {
+                    var _this = this;
+                    if (confirm("Are you sure you want to delete " + user.name + "?")) {
+                        var index = this.users.indexOf(user);
+                        this.users.splice(index, 1);
+                        this._service.deleteUser(user.id)
+                            .subscribe(null, function (err) {
+                            alert("Could not delete the user.");
+                            _this.users.splice(index, 0, user);
+                        });
+                    }
+                };
                 UsersComponent = __decorate([
                     core_1.Component({
                         templateUrl: 'app/users.component.html',
